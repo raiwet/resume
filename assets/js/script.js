e5fa8ede599c144c60e9192a999a3ad68cf4db69
Michael Raiwet <Michael@michaels-macbook-pro.local>
2022-10-23 00:00:41 +0000

    Minor revision of codebase

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const toTop = document.querySelector(".footer");
 
 window.addEventListener("scroll", () => {
-	if (window.pageYOffset > 150) {
+	if (window.pageYOffset > 400) {
 		toTop.classList.add("active");
 	} else {
 		toTop.classList.remove("active");
